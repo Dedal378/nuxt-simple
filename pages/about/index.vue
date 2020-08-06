6dcad4346620fbd86305421e3331ea10c8dabbc0
Dedal378 <orlovetsfoto@gmail.com>
2020-08-06 07:57:59 +0300

    /////

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -5,7 +5,6 @@
     <h1>About Page</h1>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, doloribus enim nihil quam quasi sunt.</p>
-
     <p>Architecto, atque ipsam minima obcaecati porro quas quis quos reprehenderit repudiandae similique veniam vero
       voluptate!</p>
 
